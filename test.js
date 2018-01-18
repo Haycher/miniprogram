6e3125bcc9e66bcafb22941dacfba8ad15d24732
spring_falling <spring_falling@163.com>
2018-01-18 18:07:15 +0800

    make it easy to use

--- test.js
+++ test.js
@@ -1,28 +1,19 @@
-const swiper = require("../../utils/swiper.scale.js")
+const swiper = require("../../utils/ft.scale.swiper.js")
 var mySwiper;
 Page({
   data: {
     swiper: {
-      number: 6,         //幻灯片数量
-      leftShow: 15,      //左侧幻灯片露出15px
-      itemGap: 50,       //放大前幻灯片间隙50px
-      itemScaleGap: 15,  //放大后幻灯片间隙15px
-      index: 0           //默认显示第一张幻灯片
+      number: 6,
+      leftShow: 15,
+      itemGap: 50,
+      itemScaleGap: 15,
+      index: 0
     },
-    randerData: [1,2,3,4,5,6]//用于渲染幻灯片的数据
+    randerData: [1,2,3,4,5,6]
   },
   onLoad: function () {
-    mySwiper = new swiper.ScaleSwiper('swiper', this);//初始化幻灯片，将对应setting参数名和this传入
-  },
-  touchstart: function(evt){
-  	mySwiper.touchstart(evt);
-  },
-  touchmove: function(evt){
-  	mySwiper.touchmove(evt);
-  },
-  touchend: function(evt){
-  	mySwiper.touchend(evt, function(){//幻灯片切换事件在这里回调
-  		console.log(mySwiper.index);//当前幻灯片索引
-  	});
+    mySwiper = new swiper.ScaleSwiper('swiper', this, function(){//初始化幻灯片，将对应setting参数名、this、callback(可不传) 传入
+      console.log(this.index);
+    });
   }
 })
